0a5dd480d17a8628a24a891e469f4d965128b595
Typas Liao <typascake@gmail.com>
2020-08-18 11:45:50 +0800

    ch20.1 reading the request

--- web-server/single-thread/src/lib.rs
+++ web-server/single-thread/src/lib.rs
@@ -1,13 +1,23 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 pub fn tcp_listening() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
 
 #[cfg(test)]
