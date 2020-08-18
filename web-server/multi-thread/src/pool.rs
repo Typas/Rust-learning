e69ed2e96292371eed5f64f567ca964993630bd2
Typas Liao <typascake@gmail.com>
2020-08-19 02:38:23 +0800

    ch20.2 Validating the Number of Threads in `new`

--- web-server/multi-thread/src/pool.rs
+++ web-server/multi-thread/src/pool.rs
@@ -1,7 +1,16 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
-    pub fn new(_size: usize) -> ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
         ThreadPool
     }
 
